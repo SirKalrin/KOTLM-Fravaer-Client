109de65df794c04ff8d4a57dde3832342e668eee
Noah Bock <noah0013@easv365.dk>
2016-11-28 21:21:24 +0100

    Noah fixed the first commit

---
+++ ServiceGateways/Entities/Absence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceGateways.Entities
+{
+    public class Absence
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public enum Status
+        {
+            S, HS, F, HF, FF, HFF, K, B, BS, AF, A, HA, SN
+        }
+
+    }
+}
---
+++ ServiceGateways/Entities/DeptChief.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceGateways.Entities
+{
+    public class DeptChief : Employee
+    {
+
+    }
+}
