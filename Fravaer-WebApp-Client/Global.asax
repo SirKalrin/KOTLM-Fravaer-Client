d6c70b4cd913a8fba8b6fd8e303dba678c606cf4
Greis <mort5240@gmail.com>
2016-11-28 11:44:40 +0100

    Initial commit

---
+++ Fravaer-WebApp-Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fravaer_WebApp_Client.MvcApplication" Language="C#" %>
---
+++ Fravaer-WebApp-Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fravaer_WebApp_Client.Startup))]
+namespace Fravaer_WebApp_Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
