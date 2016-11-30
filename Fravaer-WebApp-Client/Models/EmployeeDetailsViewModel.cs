b87ee9d2f6f42bf4dc251a6f4b23dbbcd8a59120
NicolaiMensel <nicolai.m.jorgensen@gmail.com>
2016-11-30 10:58:03 +0100

    UserDetailsView, StyleSheet and ViewModel has been added.

---
+++ Fravaer-WebApp-Client/Models/EmployeeDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DateTimeExtensions.Export;
+using ServiceGateways.Entities;
+
+namespace Fravaer_WebApp_Client.Models
+{
+    public class EmployeeDetailsViewModel
+    {
+        public User User { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+
+
+
+
+    
+}
--- ServiceGateways/Entities/Absence.cs
+++ ServiceGateways/Entities/Absence.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServiceGateways.Entities
 {
+    public enum Statuses
+    {
+        S, HS, F, HF, FF, HFF, K, B, BS, AF, A, HA, SN
+    }
+
     public class Absence : AbstractEntity
     {
         public DateTime Date { get; set; }
-        public enum Status
-        {
-            S, HS, F, HF, FF, HFF, K, B, BS, AF, A, HA, SN
-        }
-
+        public Statuses Status { get; set; }
+        
     }
 }
